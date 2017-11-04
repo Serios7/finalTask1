674110a39f3d11398aa7627f16d672bc8b9b764a
Serios7 <Serios601@gmail.com>
2017-11-04 10:04:59 +1000

    INIT

---
+++ main.py
@@ -0,0 +1,36 @@
+# 3.Дан файл с каким-то математическим выражением,
+# которое содержит скобки разных типов “{[()]}” в любом порядке.
+# Проверить сбалансированны ли скобки.
+# В случае сбалансированности вывести результат вычисления выражения,
+# иначе вывести сообщение об ошибке.
+
+
+
+
+def isBalanced(brackets):
+    stack = []
+    balanced = True
+    for index, bracket in enumerate(brackets):
+        if bracket is '{[(':
+            stack.append(bracket)
+        else:
+            if not stack:
+                balanced = False
+            else:
+                openBracket = stack.pop()
+                if not isMatches(openBracket, bracket):
+                    balanced = False
+        if not balanced:
+            break
+
+    if balanced and not stack:
+        return True
+    else:
+        return False
+
+def isMatches (openBracket, closeBracket):
+    openBrackets = '{[('
+    closeBrackets = '}])'
+    return openBrackets.index(openBracket) == closeBrackets.index(closeBracket)
+
+print(isBalanced('()(){}[]'))
