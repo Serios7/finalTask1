4cc101cbd746bb2d5654ce5f790a99315eb61a85
Serios7 <Serios601@gmail.com>
2017-11-08 23:47:14 +1000

    INIT

--- bracket.py
+++ bracket.py
@@ -5,21 +5,18 @@ BRACKETS = {'{': '}', '[': ']', '(': ')'}
 
 def is_balanced(sequence_brackets):
     stack = []
-    balanced = True
-    for index, bracket in enumerate(sequence_brackets):
+    for bracket in sequence_brackets:
         if bracket in BRACKETS.keys():
             stack.append(bracket)
         else:
             if not stack:
-                balanced = False
+                return False
             else:
                 open_bracket = stack.pop()
                 if bracket != BRACKETS.get(open_bracket):
-                    balanced = False
-        if not balanced:
-            break
+                    return False
 
-    if balanced and not stack:
+    if not stack:
         return True
     else:
         return False
